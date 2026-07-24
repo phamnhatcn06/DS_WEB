fe122a1920cb66cadc0cbcbbbbd837fc47c611eb
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-07-24 16:21:00 +0700

    Auto-commit: 2026-07-24_16:21:00

--- cms/protected/config/db.php
+++ cms/protected/config/db.php
@@ -6,8 +6,18 @@
  */
 
 return array(
-    'connectionString' => 'mysql:host=localhost;port=3306;dbname=dsh_cms',
-    'username'         => 'root',
-    'password'         => '123456a@',
-    'charset'          => 'utf8mb4',
+    //'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
+    // uncomment the following lines to use a MySQL database
+    'connectionString' => 'mysql:host=localhost;dbname=dsh_cms',
+    'emulatePrepare' => true,
+    'username' => 'root',
+    'password' => '123456a@',
+    //'schemaCachingDuration'=>3600, // number of seconds
+    //'password' => 'emga123',
+    'charset' => 'utf8',
+    'enableParamLogging' => true,
+    'enableProfiling' => true,
+    'initSQLs' => array(
+        "SET time_zone = '+07:00'",
+    ),
 );
---
+++ cms/protected/models/Partner.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Đối tác / cổ đông chiến lược (Section 8).
+ */
+class Partner extends BaseActiveRecord
+{
+    public static function typeOptions()
+    {
+        return array(
+            'partner'     => 'Đối tác',
+            'shareholder' => 'Cổ đông',
+            'regulator'   => 'Cơ quan quản lý',
+        );
+    }
+
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return 'partners';
+    }
+
+    public function rules()
+    {
+        return array(
+            array('name', 'required', 'message' => 'Tên đối tác không được để trống.'),
+            array('name', 'length', 'max' => 255),
+            array('website_url', 'length', 'max' => 500),
+            array('website_url', 'url', 'allowEmpty' => true,
+                'message' => 'Địa chỉ website không hợp lệ.'),
+            array('partner_type', 'in', 'range' => array_keys(self::typeOptions()),
+                'message' => 'Loại đối tác không hợp lệ.'),
+            array('ownership_percent', 'numerical', 'min' => 0, 'max' => 100,
+                'tooSmall' => 'Tỷ lệ sở hữu phải từ 0 đến 100.',
+                'tooBig'   => 'Tỷ lệ sở hữu phải từ 0 đến 100.'),
+            array('logo_media_id, sort_order', 'numerical', 'integerOnly' => true),
+            array('is_active', 'boolean'),
+        );
+    }
+
+    public function relations()
+    {
+        return array(
+            'logo' => array(self::BELONGS_TO, 'MediaFile', 'logo_media_id'),
+        );
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+            'name'              => 'Tên đối tác',
+            'logo_media_id'     => 'Logo',
+            'website_url'       => 'Website',
+            'partner_type'      => 'Phân loại',
+            'ownership_percent' => 'Tỷ lệ sở hữu (%)',
+            'sort_order'        => 'Thứ tự',
+            'is_active'         => 'Hiển thị',
+        );
+    }
+
+    public function getTypeLabel()
+    {
+        $options = self::typeOptions();
+        return isset($options[$this->partner_type])
+            ? $options[$this->partner_type]
+            : $this->partner_type;
+    }
+}
