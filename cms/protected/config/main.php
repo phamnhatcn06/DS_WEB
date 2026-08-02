c10add65a1f8aab8de67ddf63bf855cdbaadd1ff
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-02 16:57:59 +0700

    Auto-commit: 2026-08-02_16:57:59

--- cms/protected/config/main.php
+++ cms/protected/config/main.php
@@ -105,6 +105,8 @@ return array(
                 // Trang chủ mặc định: '/' vào thẳng module frontend.
                 ''                                       => 'frontend/home/index',
                 'du-lieu-trang-chu'                      => 'frontend/home/dataSource',
+                // Trang lưu trữ theo thẻ: liệt kê tin tức + lĩnh vực gắn thẻ.
+                'the/<slug:[a-z0-9\-]+>'                 => 'frontend/tag/view',
                 '<action:(error|contact)>'               => 'site/<action>',
             ),
         ),
