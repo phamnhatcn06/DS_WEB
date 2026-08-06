1da74e28b1b7589e3f340e472ea72a969507cde3
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-06 16:48:49 +0700

    Auto-commit: 2026-08-06_16:48:49

--- cms/protected/models/NewsPost.php
+++ cms/protected/models/NewsPost.php
@@ -7,6 +7,9 @@ class NewsPost extends BaseActiveRecord
     /** @var int[]|null id các thẻ được chọn trong form (trường ảo, không phải cột). */
     private $_tagIds;
 
+    /** @var int[]|null id các danh mục được chọn trong form (trường ảo, không phải cột). */
+    private $_categoryIds;
+
     /**
      * Kích thước card trong lưới tin của trang chủ.
      * Thiết kế có 3 ô khác nhau: 1 card lớn, 1 card cao, các card nhỏ.
