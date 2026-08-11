fb01c7b9c73757aaaf3a90da67f29fd27ae7d743
phamnhatcn06 <phamnhat.cn06@gmail.com>
2026-08-12 01:53:10 +0700

    Auto-commit: 2026-08-12_01:53:10

--- cms/protected/commands/ImportWxrCommand.php
+++ cms/protected/commands/ImportWxrCommand.php
@@ -87,7 +87,7 @@ class ImportWxrCommand extends CConsoleCommand
 
         // Bước 2: duyệt bài viết.
         $stat = array('imported' => 0, 'updatedCats' => 0, 'skippedExisting' => 0,
-            'skippedNoCat' => 0, 'skippedNotPublish' => 0, 'skippedForeign' => 0,
+            'fallbackCat' => 0, 'skippedNotPublish' => 0, 'skippedForeign' => 0,
             'images' => 0, 'errors' => 0);
         $processed = 0;
 
